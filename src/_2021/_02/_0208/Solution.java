027b9e61c404168d3d100981f61fba9ae5b2cf7e
ChenforCode <727949427@qq.com>
2021-02-08 21:55:04 +0800

    最长湍流子数组

---
+++ src/_2021/_02/_0208/Solution.java
@@ -0,0 +1,33 @@
+package _2021._02._0208;
+
+/**
+ * @author <a href="http://blog.chenforcode.cn">PKUCoder</a>
+ * @date 2021/2/8 9:54 下午
+ * @description 978. 最长湍流子数组
+ */
+public class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+        int n = arr.length;
+        int ret = 1;
+        int left = 0, right = 0;
+
+        while (right < n - 1) {
+            if (left == right) {
+                if (arr[left] == arr[left + 1]) {
+                    left++;
+                }
+                right++;
+            } else {
+                if (arr[right - 1] < arr[right] && arr[right] > arr[right + 1]) {
+                    right++;
+                } else if (arr[right - 1] > arr[right] && arr[right] < arr[right + 1]) {
+                    right++;
+                } else {
+                    left = right;
+                }
+            }
+            ret = Math.max(ret, right - left + 1);
+        }
+        return ret;
+    }
+}
