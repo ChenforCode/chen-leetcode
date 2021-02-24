cfdb458c554bf6d65a4ce6a9b1fe3b3d33c1f66c
ChenforCode <727949427@qq.com>
2021-02-24 12:22:11 +0800

    反转图像

---
+++ src/leetcode/editor/cn/[832]翻转图像.java
@@ -0,0 +1,69 @@
+package leetcode.editor.cn;
+
+//给定一个二进制矩阵 A，我们想先水平翻转图像，然后反转图像并返回结果。 
+//
+// 水平翻转图片就是将图片的每一行都进行翻转，即逆序。例如，水平翻转 [1, 1, 0] 的结果是 [0, 1, 1]。 
+//
+// 反转图片的意思是图片中的 0 全部被 1 替换， 1 全部被 0 替换。例如，反转 [0, 1, 1] 的结果是 [1, 0, 0]。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：[[1,1,0],[1,0,1],[0,0,0]]
+//输出：[[1,0,0],[0,1,0],[1,1,1]]
+//解释：首先翻转每一行: [[0,1,1],[1,0,1],[0,0,0]]；
+//     然后反转图片: [[1,0,0],[0,1,0],[1,1,1]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：[[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+//输出：[[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+//解释：首先翻转每一行: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]]；
+//     然后反转图片: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= A.length = A[0].length <= 20 
+// 0 <= A[i][j] <= 1 
+// 
+// Related Topics 数组 
+// 👍 226 👎 0
+
+class FlippingAnImage {
+    public static void main(String[] args) {
+        Solution solution = new FlippingAnImage().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        int len = A.length;
+        for (int i = 0; i < len; i++) {
+            int left = 0, right = len - 1;
+            while (left < right) {
+                if (A[i][left] == A[i][right]) {
+                    A[i][left] ^= 1;
+                    A[i][right] ^= 1;
+                }
+                left++;
+                right--;
+            }
+            if (left == right) {
+                A[i][left] ^= 1;
+            }
+        }
+        return A;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
