b11062f4836eaecaa0757396434e215698793058
ChenforCode <727949427@qq.com>
2021-03-05 18:06:20 +0800

    双栈实现队列

---
+++ src/offer/[7]重建二叉树.java
@@ -0,0 +1,63 @@
+package offer;
+
+import java.util.Arrays;
+
+/**
+ * @author <a href="http://blog.chenforcode.cn">PKUCoder</a>
+ * @date 2021/3/3 9:05 下午
+ * @description 剑指 Offer 07. 重建二叉树
+ *
+ * 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ *
+ *  
+ *
+ * 例如，给出
+ *
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 返回如下的二叉树：
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *  
+ *
+ * 限制：
+ *
+ * 0 <= 节点个数 <= 5000
+ *
+ *  
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zhong-jian-er-cha-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class BuildTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int rootVal = preorder[0];
+        int rootIndex = 0;
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == rootVal) {
+                rootIndex = i;
+            }
+        }
+
+        TreeNode root = new TreeNode(0);
+        root.left = buildTree(Arrays.copyOfRange(preorder, ));
+        root.right = buildTree();
+        return root;
+    }
+
+}
